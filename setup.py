944c55d1ad2fa82f664b0a8b3d9bf85d02cc3ec8
Sotirios M. Roussis <xtonousou@gmail.com>
2020-09-12 20:07:39 +0300

    Added aggressive URL encode flag

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='rshelly',  
-    version='0.4',
+    version='0.5',
     scripts=['rshelly', ],
     author='Sotirios Roussis a.k.a. xtonousou',
     author_email='sroussis@xtonousou.xyz',
